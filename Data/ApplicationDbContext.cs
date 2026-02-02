85665720efe036be659b3331286eed3155614e1d
ehamzaj21-ship-it <ehamzaj21@epoka.edu.al>
2026-02-03 00:51:15 +0100

    Add ApplicationDbContext class to manage User records

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Emanuel_WebApp.Models;
+
+namespace Emanuel_WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
