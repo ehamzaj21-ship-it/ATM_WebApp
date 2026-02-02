df13d6148b534c753605c74ec6d40a31119b3b2c
ehamzaj21-ship-it <ehamzaj21@epoka.edu.al>
2026-02-03 00:45:21 +0100

    Create HomeController.cs file

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emanuel_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
